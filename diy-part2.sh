53327a664fe6122dbad646d72825953cb4a2e8ed
zuifengguai <46639423+zuifengguai@users.noreply.github.com>
2022-05-26 13:06:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,8 @@
 # 更改默认IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+# 更改默认主机名
+sed -i 's/hostname 'OpenWrt'/hostname='ZUIKE'/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/' feeds/luci/collections/luci/Makefile
