c710cf6ab30ac7dd27937dfde52445146a5af492
zuifengguai <46639423+zuifengguai@users.noreply.github.com>
2022-07-06 13:21:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,10 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
 # Add a feed source 第三方源
 #echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 #echo 'src-git smpackage https://github.com/kenzok78/small-package' >>feeds.conf.default
@@ -27,81 +23,45 @@ echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.defa
 git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-argonne
 
 ##############################################################################################
-
+##############################################################################################
+# 第六版本功能
+# open-vm-tools   #支持虚拟化
+#####################
 # 状态
-
-# 实时监控 lede包已有
-
-#释放内存 lede包已有
-
-##############################################################################################
-
+# luci-app-firewall   #添加防火墙
+# luci-app-ramfree  #释放内存
+#####################
 #  系统
-
-#定时重启  luci-app-autoreboot
-
-#ttyd lede包已有
-
-#磁盘管理  磁盘管理插件luci-app-diskman
-
-
-#文件传输 lede包已有
-
-#  关机   luci-app-poweroff
-
-#添加防火墙luci-app-firewall 
-
-#网络带宽监视器luci-app-nlbwmon
-
-#网络共享（Samba）luci-app-samba
-
-#Turbo ACC 网络加速(支持 Fast Path 或者 硬件 NAT)
-
-#ttyd
-
-#重启advanced-reboot
-
-#实时监控 luci-app-netdata    
-
-#网络访问控制luci-app-control-weburl
-
-#Argon 主题设置 argonne主题设置    luci-app-argonne-config
-
-
-##############################################################################################
-
+# luci-app-diskman  #DiskMan 磁盘管理
+# luci-app-ttyd     #ttyd
+# luci-app-netdata   #Netdata实时监控
+# advanced-reboot  #重启
+# luci-app-autoreboot  #定时重启  
+# luci-app-poweroff    #关机
+#####################
+#istore
+# luci-app-store   #应用商店
+#####################
+#docker
+# luci-app-dockerman  #docker
+#####################
 # 服务
-
-#docker   luci-app-dockerman
-
-#AdGuard home广告过滤（Le库以外的插件）luci-app-adguardhome 
-
-#ShadowSocksR Plus+
-
-#passwall       luci-app-passwall   
-
-#OpenClash   openclash
-
-#adguardhome  luci-app-adguardhome
-
-#SmartDNS    luci-app-smartdns
-
-#应用商店   luci-app-store
-
-##############################################################################################
-
-# NAS
-
-#文件助手   luci-app-fileassistant
-
-#USB 打印服务器luci-app-usb-printer
-
-##############################################################################################
-
+# luci-app-adguardhome   #adguardhome广告过滤
+# luci-app-openclash	#OpenClash 
+# luci-app-passwall   #passwall
+# luci-app-smartdns  #SmartDNS 服务器
+# luci-app-fileassistant  # web文件助手 
+# luci-app-ssr-plus  #ShadowSocksR Plus+  
+#####################
 # 网络
+# luci-app-usb-printer  #USB 打印服务器
+# luci-app-sfe   #Turbo ACC网络加速
+# luci-app-samba  #网络共享（Samba）
+##############################################################################################
+##############################################################################################
 
-#Turbo ACC 加速 lede包已有
-
+#以下为原来版本更新内容
+##############################################################################################
 ##############################################################################################
 #########第四版本升级项目[已编译]
 #删     定时重启、定时关机、定时重启网络.   luci-app-autotimeset  
@@ -127,5 +87,8 @@ git clone https://github.com/kenzok78/luci-theme-argonne.git package/luci-theme-
 #删     FTP 服务器  luci-app-vsftpd
 #删     实时流量监测  luci-app-nlbwmon
 ##############################################################################################
-#########第6版即将更新[未编译]
+#########第6版即将更新[已编译]
 #加     磁盘管理插件：luci-app-diskman
+#加     passwall       luci-app-passwall   
+#删     HelloWorld  luci-app-vssr/
+#删     支持IPv6
