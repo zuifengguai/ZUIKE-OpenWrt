cad4b5ed70e7afef8a60ee48f3feb5c2c8eb17cb
zuifengguai <46639423+zuifengguai@users.noreply.github.com>
2022-07-08 10:57:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,28 @@
 #
 
 # 更改默认IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/' feeds/luci/collections/luci/Makefile
 
+#添加编译作者
+sed -i '/<tr><td width="33%"><%:CPU usage/a <tr><td width="33%"><%:Compiler author%></td><td>zuifengguai</td></tr>' package/lean/autocore/files/x86/index.htm
+#修改主机名
+sed -i 's/OpenWrt/ZUIKE/g' package/base-files/files/bin/config_generate
+#添加汉化
+sed -i '5a\msgid "Compiler author"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '6a\msgstr "编译作者"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '7a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#添加汉化
+
+#添加固件更新地址
+sed -i '/<tr><td width="33%"><%:Compiler author/a <tr><td width="33%"><%:Firmware Update%></td><td><a target="_blank" href="https://zuike7.com">稳定版点这里</a></td></tr>' package/lean/autocore/files/x86/index.htm
+#添加汉化
+sed -i '8a\msgid "Firmware Update"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '9a\msgstr "固件更新"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '10a \\' feeds/luci/modules/luci-base/po/zh-cn/base.po
+#添加汉化
+
+#修复依赖
+sed -i 's/PKG_HASH.*/PKG_HASH:=skip/' feeds/packages/utils/containerd/Makefile
