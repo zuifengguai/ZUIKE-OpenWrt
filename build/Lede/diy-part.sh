dfaa5c2187230f30d934aa0f91374341d061d441
zuifengguai <46639423+zuifengguai@users.noreply.github.com>
2025-03-13 19:35:27 +0800

    测试lede

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -4,10 +4,10 @@
 # 自行拉取插件之前请SSH连接进入固件配置里面确认过没有你要的插件再单独拉取你需要的插件
 # 不要一下就拉取别人一个插件包N多插件的，多了没用，增加编译错误，自己需要的才好
 
-#添加luci-app-mosdns插件
-git clone -b v5-lua --depth 1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-#添加luci-app-pushbot全能推送插件
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
+# 添加luci-app-mosdns插件
+# git clone -b v5-lua --depth 1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
+# 添加luci-app-pushbot全能推送插件
+# git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 
 # 后台IP设置
 export Ipv4_ipaddr="192.168.5.1"            # 修改openwrt后台地址(填0为关闭)
